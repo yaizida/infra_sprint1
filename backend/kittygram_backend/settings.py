eac1165c7a683d632836ff411d911c16ab8725c3
izida <109024816+yaizida@users.noreply.github.com>
2023-08-31 18:52:37 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG')
+DEBUG = bool(os.getenv("DEBUG")) 
 
 ALLOWED_HOSTS = os.getenv('HOSTS', '').split(',')
 
